cec6cb8024c7388263f5702255ea7e51da12844c
RashikaMishra95 <rashika.mishra112@gmail.com>
2019-07-24 11:58:27 +0530

    change in conn

--- config/conn.js
+++ config/conn.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 const Operation = Sequelize.Op;
 
-exports.mysql = new Sequelize('db_graphql','root','',{
-    host:'localhost',
+exports.mysql = new Sequelize('db_graphql','developer','developer',{
+    host:'192.168.0.102',
     port:3306,
     dialect:'mysql',
     operatorsAliases:Operation
